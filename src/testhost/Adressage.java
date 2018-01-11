90708cc689b9ea110194cb34a8a56b3e10e2552d
sledezma.cpi2019 <sledezma.cpi2019@esaip.org>
2018-01-11 11:27:12 +0100

    First example

---
+++ src/testhost/Adressage.java
@@ -0,0 +1,26 @@
+package testhost;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Adressage {
+
+
+	public static void main(String[] args) {
+		InetAddress LocaleAdresse ;
+		InetAddress ServeurAdresse;
+		try {
+			LocaleAdresse = InetAddress.getLocalHost();
+			System.out.println("locale = " + LocaleAdresse); 
+			//source de l'exemple
+			ServeurAdresse =
+					InetAddress.getByName(
+							"www.openclassrooms.com"
+							);
+			System.out.println("oc = "+ServeurAdresse);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+
+}

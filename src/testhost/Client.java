a85537dbeab327cf4d8f31969b249ca4e247b13d
sledezma.cpi2019 <sledezma.cpi2019@esaip.org>
2018-01-11 13:54:05 +0100

    Client/Serveur
    Transmission de données

--- src/testhost/Client.java
+++ src/testhost/Client.java
@@ -1,21 +1,29 @@
 package testhost;
 
-import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class Client {
 	//source Open Classroom
 	public static void main(String[] args) {
-		Socket socket;
 		try {
-			socket = new Socket(InetAddress.getLocalHost(),2017);
-			System.out.println("Connexion réussi.");
-			socket.close();
-		}catch (UnknownHostException e) {
-			e.printStackTrace();
-		}catch (IOException e) {
+			// ouverture d'une connexion TCP
+			Socket socket= new Socket(InetAddress.getLocalHost(), 5000);
+			ObjectOutputStream oos = new ObjectOutputStream( socket.getOutputStream());
+			// envoi vers le serveur de cette «requête»
+			SortedSet<String> l = new TreeSet<String>();
+			l.add("TCP");
+			l.add("essai");
+			oos.writeObject( l);
+			// lecture de la réponse retournée
+			ObjectInputStream ois =	new ObjectInputStream( socket.getInputStream());
+			System.out.println("le serveur retourne : " + ois.readObject());
+			socket.close(); 
+		}catch (Exception e){
 			e.printStackTrace();
 		}
 	}
--- src/testhost/Serveur.java
+++ src/testhost/Serveur.java
@@ -1,21 +1,32 @@
 package testhost;
 
-import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 //source Open Classroom
 public class Serveur {
 	public static void main(String[] args) {
-		ServerSocket socketserver  ;
-		Socket socketduserveur ;
-		try {
-			socketserver = new ServerSocket(2017);
-			socketduserveur = socketserver.accept(); 
-			System.out.println("un client connecté");
-			socketserver.close();
-			socketduserveur.close();
-		}catch (IOException e) {
+		try{
+			ServerSocket serveur= new ServerSocket(5000);
+			boolean fini=false;
+			while(!fini) {
+				Socket socket= serveur.accept();
+				// attente active d’un client
+				// récupération de la requête
+				ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+				Object obj= ois.readObject();
+				System.out.println("le serveur a reçu : " + obj);
+				// envoi de la réponse
+				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+				fini=(((int)(Math.random()*5))==0);
+				oos.writeObject(obj.toString()+fini);
+				socket.close();
+			}
+			System.out.println("fermeture du serveur");
+			serveur.close();
+		}catch (Exception e){
 			e.printStackTrace();
 		}
 	}
